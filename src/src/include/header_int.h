a94245e2e337405763cb4c6d4ecfb508a4c4ef94
Fannie <pengya06101@163.com>
2016-06-13 23:49:33 +0800

    init

---
+++ src/src/include/header_int.h
@@ -0,0 +1,15 @@
+/*!
+ * Copyrights
+ *
+ * Portions created or assigned to Cisco Systems, Inc. are
+ * Copyright (c) 2014-2016 Cisco Systems, Inc.  All Rights Reserved.
+ */
+
+#ifndef SRC_HEADER_INT_H
+#define SRC_HEADER_INT_H
+
+// extern const char *CJOSE_HDR_ATTRS[];
+
+#define snprintf _snprintf;
+
+#endif // SRC_HEADER_INT_H
